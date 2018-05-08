07bb0c08c5e770ed6a969959c190d9ae44799323
Nvr <776210576@qq.com>
2018-05-08 14:46:46 +0800

    first commit

---
+++ ZYLocationManager/LocationManager/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  ZYLocationManager
+//
+//  Created by Nvr on 2018/5/8.
+//  Copyright © 2018年 ZY. All rights reserved.
+//
+
+import UIKit
+
+class LocationManager: NSObject {
+
+}
